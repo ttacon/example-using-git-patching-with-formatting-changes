727c9b16448d50fa41002fae9fe39e07656673eb
Trey Tacon <ttacon@gmail.com>
2019-12-14 10:43:32 -0500

    feat: add example.js

---
+++ example.js
@@ -0,0 +1,19 @@
+const _ = require("underscore");
+
+/**
+ * Example file showing how to split unstaged git changes into two commits.
+ *
+ * Yes, this is a contrived example 😝.
+ */
+
+function exampleCall({ nameObj } = {}) {
+  const retObj = { ..._.omit(nameObj, "firstName", "lastName"), firstName: nameObj.firstName, lastName: nameObj.lastName };
+
+  if (!nameObj.firstName && !nameObj.lastName) {
+    retObj.anObjectHasNoName = true;
+  }
+
+  return retObj;
+}
+
+module.exports = { exampleCall };
